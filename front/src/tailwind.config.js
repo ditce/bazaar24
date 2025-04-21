f1dd4a026c7f20d91986d26208ecd2bf5c460472
Algert Qato <algertqato03@gmail.com>
2025-04-21 16:21:21 +0200

    last check

--- front/src/tailwind.config.js
+++ front/src/tailwind.config.js
@@ -1,20 +1,17 @@
-/* eslint-disable no-undef */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'lavender': '#D6A9F0',
-        'light-grey': '#E1E1E1',
-        'soft-white': '#FFFFFF',
-        'blue-light': '#A6C8FF',
+        lavender: "#D6A9F0",
+        "light-grey": "#E1E1E1",
+        "soft-white": "#FFFFFF",
+        "blue-light": "#A6C8FF",
       },
       backgroundImage: {
-        'gradient-light': 'linear-gradient(to bottom, #D6A9F0, #A6C8FF)',
+        "gradient-light": "linear-gradient(to bottom, #D6A9F0, #A6C8FF)",
       },
     },
   },
   plugins: [],
-}
+};
