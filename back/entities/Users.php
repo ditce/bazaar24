6435109025b46b8335e5ff4da549e7e750da3e85
ditce <arditxhuveli@gmail.com>
2025-04-21 17:33:27 +0200

    add findByEmail method

--- back/entities/Users.php
+++ back/entities/Users.php
@@ -3,6 +3,19 @@ require_once './BaseModel.php';
 
 class User extends Model {
     protected $table = 'users';
+
+    public function findByEmail($email) {
+        $pdo = Database::connect();
+        $sql = 'SELECT * FROM users WHERE email = :email LIMIT 1';
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindValue('email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $user ? new static($user) : 'No user found';
+    }
 }
 
 ?>
